b8cceb49250b34e9313f85722a95e4240328946a
Maryline-Bellanger <maryline.bellanger45@gmail.com>
2022-09-19 11:50:31 +0200

    add all modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,37 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/sign-in', pathMatch: 'full' },
+
+  {
+    path: '',
+    loadChildren: () =>
+      import('./login/login.module').then((m) => m.LoginModule),
+  },
+
+  {
+    path: 'orders',
+    loadChildren: () =>
+      import('./orders/orders.module').then((m) => m.OrdersModule),
+  },
+  {
+    path: 'clients',
+    loadChildren: () =>
+      import('./clients/clients.module').then((m) => m.ClientsModule),
+  },
+  {
+    path: '**',
+    loadChildren: () =>
+      import('./page-not-found/page-not-found.module').then((m) => m.PageNotFoundModule),
+  },
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,
+    {preloadingStrategy: PreloadAllModules}
+    )],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
---
+++ src/app/ui/components/ui/ui.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ui',
+  templateUrl: './ui.component.html',
+  styleUrls: ['./ui.component.scss']
+})
+export class UiComponent implements OnInit {
+
+  public open: boolean = true;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  public onSubmit(){
+    //console.log('cliqué');
+    //faire alterner open de true à false
+
+    this.open = !this.open;
+    console.log(this.open);
+  }
+
+} // ne rien écrire après cette ligne
